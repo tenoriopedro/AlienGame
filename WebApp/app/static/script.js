6a5265119cde03c8e6d036637e5e21fa59949a74
tenoriopedro <pstsouza13@gmail.com>
2024-12-18 00:26:53 +0000

    Removing files

--- WebApp/app/static/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menuToggle');
-const filter = document.querySelector('.filter');
-
-menuToggle.onclick = () => {
-    filter.classList.toggle('active');
-}
